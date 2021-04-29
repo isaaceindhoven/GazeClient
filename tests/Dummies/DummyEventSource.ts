21d107e396fe77e0a15528ea7caffc39a8032b0e
Bader Ali <bader.ali@isaac.nl>
2021-04-29 16:42:39 +0200

    Add copyright comment

--- tests/Dummies/DummyEventSource.ts
+++ tests/Dummies/DummyEventSource.ts
@@ -1,3 +1,12 @@
+/**
+  *   Do not remove or alter the notices in this preamble.
+  *   This software code regards ISAAC Standard Software.
+  *   Copyright © 2021 ISAAC and/or its affiliates.
+  *   www.isaac.nl All rights reserved. License grant and user rights and obligations
+  *   according to applicable license agreement. Please contact sales@isaac.nl for
+  *   questions regarding license and user rights.
+  */
+
 import { SseClient } from '../../src/SseClient';
 
 class DummyEventSource implements SseClient {
