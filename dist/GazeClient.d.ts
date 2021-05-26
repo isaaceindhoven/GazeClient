f0b76aceccdc51a71342efd1cc027f81e8cd0ad4
Bader Ali <bader.ali@isaac.nl>
2021-05-26 14:30:04 +0200

    0.5.0

---
+++ dist/GazeClient.d.ts
@@ -0,0 +1,26 @@
+import { Callback } from './Types';
+import { GazeRequestor } from './Requestors/GazeRequestor';
+import { SseClient } from './SseClient';
+declare class GazeClient {
+    private hubUrl;
+    private sseClient;
+    private gazeRequestor;
+    private connected;
+    private subscriptions;
+    private middlewareList;
+    private token;
+    onConnectionReset: null | (() => void);
+    constructor(hubUrl: string, sseClient?: SseClient, gazeRequestor?: GazeRequestor);
+    connect(): Promise<GazeClient>;
+    on<T>(topics: string | string[] | (() => string[]), callback: Callback<T>): Promise<{
+        update: () => void;
+        destroy: () => void;
+    }>;
+    private update;
+    private destroy;
+    addMiddleware(handler: (payload: unknown, next: ((newPayload: unknown) => void)) => void): void;
+    authenticate(token: string): Promise<void>;
+    private reconnect;
+    disconnect(): void;
+}
+export default GazeClient;
